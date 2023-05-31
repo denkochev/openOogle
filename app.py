4db979d6cda38c7d98d3c38c23e4b71c6cf54a92
DenysWinPC <denkochev78@gmail.com>
2023-05-31 19:53:11 +0200

    Last backend version before PostgreSQL migration

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from storage_db import DBStorage
 from filter import Filter
 from search import search
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import json
 
 app = FastAPI()
@@ -15,6 +16,11 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
+
+class Ranking(BaseModel):
+    query: str
+    link: str
+    score: int
 
 def run_search(query):
     results = search(query)
@@ -31,3 +37,14 @@ def find(q: str):
     # print('USER QUERY ---->',q)
     search_results = run_search(q)
     return search_results
+
+@app.post("/evaluate")
+def mark_relevant(rank: Ranking):
+    evaluated_query = rank.query
+    evaluated_link = rank.link
+    evaluated_score = rank.score
+
+    storage = DBStorage()
+    storage.update_relevance(evaluated_query, evaluated_link, evaluated_score)
+
+    return {"success":True}
